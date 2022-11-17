de67a21cac0702140e650bd0d8bae8839313a9b0
adria <adrianhossen5@gmail.com>
2022-11-17 16:17:13 -0500

    more replies

--- events/replies.ts
+++ events/replies.ts
@@ -5,8 +5,22 @@ const Replies = {
   async execute(message: Message) {
     if (message.author.bot) return;
 
+    const lowered = message.content.toLowerCase();
+    const chance = Math.floor(Math.random() * 2) == 0;
+
+    if (chance) {
+      if (lowered.split(' ')[0] == 'bill')
+        await message.reply(
+          'https://tenor.com/view/bill-gates-dab-gif-25596097'
+        );
+      else if (lowered.split(' ')[0] == 'almond' || lowered === 'almond')
+        await message.reply('milk');
+    }
+
     if (message.client.user && message.mentions.has(message.client.user.id))
-      await message.reply('weeb');
+      await message.reply(
+        'https://tenor.com/view/clippy-microsoft-office-word-publisher-gif-5630386'
+      );
   },
 };
 
